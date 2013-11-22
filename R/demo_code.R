0fafb9c79260c72d407c6772adb0cb0da4f2bc26
Jeffrey Chrabaszcz <jchrszcz@Jeffreys-MacBook-Air.local>
2013-11-22 13:39:09 -0500

    presentation final

--- R/demo_code.R
+++ R/demo_code.R
@@ -1,9 +1,22 @@
-# RT demo file
+# uncomment and install these if first time using
+# install.packages("Rcpp")
+# install.packages("devtools")
+# install.packages("assist")
+# install.packages("signals")
+# library(devtools); install_github("gemmR", "jchrszcz", subdir = "gemmR")
 
-# 1. vanZandt's smoothing spline filter
+# load these every time, but have to install first
+library(Rcpp)
+library(signals)
+library(assist)
+library(gemmR)
+
+# load data into workspace, have to download from github
 
 load("demo.RData")
 source("vzfilter.R")
+
+# run filtering
 
 subjects <- unique(df$Subj)
 df$new.vz.filter <- df$log.rt
@@ -14,3 +27,23 @@ for (i in 1:length(subjects)) {
   df$new.vz.filter[df$Subj == subjects[i] & !is.na(df$new.vz.filter)] <- temp
 }
 
+# repeated-measures ANOVA on raw RTs
+
+new.df <- with(df, aggregate(RT ~ Center + Difference + Gain + Subj, FUN = median))
+raw.mod <- lm(RT ~ Center * Difference * Gain + Subj, data = new.df)
+anova(raw.mod)
+
+# repeated-measures ANOVA on filtered RTs
+
+vz.df <- with(df, aggregate(new.vz.filter ~ Center + Difference + Gain + Subj, FUN = median))
+vz.mod <- lm(new.vz.filter ~ Center * Difference * Gain + Subj, data = vz.df)
+anova(vz.mod)
+
+# GeMM on RTs
+
+# scaling the RTs just makes them more amenable to search, maintains orders
+new.df$RT <- as.vector(scale(new.df$RT))
+
+# run gemm
+a <- gemm(RT ~ Center * Difference * Gain + Subj, data = new.df)
+print(a)
